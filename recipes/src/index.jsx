fcc8714f8efb9036daf8c1ca43b57b292dee572a
Jeff Brower <jeffbrower0@gmail.com>
2026-04-18 18:14:02 -0400

    put recipes app in dev mode

--- recipes/src/index.jsx
+++ recipes/src/index.jsx
@@ -3,9 +3,17 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
-const container = document.getElementById("root");
-createRoot(container).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+function mount() {
+  const container = document.getElementById("root");
+  createRoot(container).render(
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", mount);
+} else {
+  mount();
+}
